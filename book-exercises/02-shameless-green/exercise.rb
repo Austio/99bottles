90a06128e7667d2ed6b2ba3fcfa1d35f54cd1024
Austin Story <astory@doximity.com>
2017-03-03 08:36:13 -0600

    Adding verse 2 solution

--- book-exercises/02-shameless-green/exercise.rb
+++ book-exercises/02-shameless-green/exercise.rb
@@ -3,10 +3,17 @@ require 'minitest/autorun'
 
 class Bottles
   def verse(number)
-    "#{number} bottles of beer on the wall, " +
-      "#{number} bottles of beer. \n " +
-      "Take one down and pass it around, " +
-      "#{number - 1} bottles of beer on"
+    if number == 2
+        "#{number} bottles of beer on the wall, " +
+        "#{number} bottles of beer. \n " +
+        "Take one down and pass it around, " +
+        "#{number - 1} bottle of beer on the wall"
+    else
+      "#{number} bottles of beer on the wall, " +
+        "#{number} bottles of beer. \n " +
+        "Take one down and pass it around, " +
+        "#{number - 1} bottles of beer on the wall"
+    end
   end
 end
 
@@ -17,7 +24,7 @@ class BottlesTest < Minitest::Test
     expected = "99 bottles of beer on the wall, " +
       "99 bottles of beer. \n " +
       "Take one down and pass it around, " +
-      "98 bottles of beer on"
+      "98 bottles of beer on the wall"
     assert_equal expected, Bottles.new.verse(99)
   end
 
@@ -25,7 +32,15 @@ class BottlesTest < Minitest::Test
     expected = "3 bottles of beer on the wall, " +
       "3 bottles of beer. \n " +
       "Take one down and pass it around, " +
-      "2 bottles of beer on"
+      "2 bottles of beer on the wall"
     assert_equal expected, Bottles.new.verse(3)
+  end
+
+  def test_verse_2
+    expected = "2 bottles of beer on the wall, " +
+      "2 bottles of beer. \n " +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall"
+    assert_equal expected, Bottles.new.verse(2)
   end
 end
