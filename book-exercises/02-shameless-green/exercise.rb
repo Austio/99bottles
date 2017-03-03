7050c9fbcab926321eecaecefd42f55ee2c1b38d
Austin Story <astory@doximity.com>
2017-03-03 07:06:57 -0600

    Green verse 3

--- book-exercises/02-shameless-green/exercise.rb
+++ book-exercises/02-shameless-green/exercise.rb
@@ -2,11 +2,11 @@ require 'minitest'
 require 'minitest/autorun'
 
 class Bottles
-  def verse(_)
-    "99 bottles of beer on the wall, " +
-      "99 bottles of beer. \n " +
+  def verse(number)
+    "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer. \n " +
       "Take one down and pass it around, " +
-      "98 bottles of beer on"
+      "#{number - 1} bottles of beer on"
   end
 end
 
